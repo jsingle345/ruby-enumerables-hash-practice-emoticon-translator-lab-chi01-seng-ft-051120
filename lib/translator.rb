8119bf524faf393f045964f0e31d41d7fdf76546
Joshua Singleton <jsingle345@gmail.com>
2020-05-03 19:49:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(file_path, emoticons)
   japanese_emoticons = " "
   
   l_library.map do |new_hash, value|
-    value.map do |key, value2|
+    value.map do |key2, value|
       if value2 == emoticons
         japanese_emoticons = new_hash
       end
