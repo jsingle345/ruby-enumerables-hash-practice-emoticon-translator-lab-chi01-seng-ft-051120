bae0c152caf14fb052a26a2cf09fc16e75e65970
Joshua Singleton <jsingle345@gmail.com>
2020-05-03 19:36:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,8 +34,8 @@ def get_japanese_emoticon(file_path, emoticons)
   end 
   japanese_emoticons
 end
-  
-  !!!
+
+
   
 def get_english_meaning(file_path, emoticons)
   sorry_message = "Sorry, that emoticon was not found"
