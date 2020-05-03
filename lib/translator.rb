8771cc7b6cc43a3ae6176552f7b34a8cb65bf79a
Joshua Singleton <jsingle345@gmail.com>
2020-05-03 18:30:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -41,7 +41,7 @@ def get_english_meaning(file_path, emoticons)
   
   l_library.map do |new_hash, value|
     value.map do |key, value2|
-      #binding.pry
+      binding.pry
       if l_library[value][key][:japanese]
         english_emoticons << l_library[value]
       end
