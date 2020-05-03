c8e196dbf03e725a3d937de51dc44b81aceeff45
Joshua Singleton <jsingle345@gmail.com>
2020-05-03 18:42:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -46,8 +46,8 @@ def get_english_meaning(file_path, emoticons)
         english_emoticons = new_hash
       end
       
-      if value2 ==! [:english]
-        puts sorry_message
+      if value2 !== emoticon
+        english_emoticons = sorry_message
       end
     end 
   end 
